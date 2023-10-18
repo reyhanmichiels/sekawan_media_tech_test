58221df8ea7cc9e030c9259d591d6398dcd28ade
reyhanmichiels <michiels.reyhan@gmail.com>
2023-10-18 21:15:16 +0700

    build: create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Rent;
+use App\Models\User;
+use App\Models\Vehicle;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,57 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //SEEDER FOR USER
+        User::create([
+            "name" => "userAdmin",
+            "role" => "admin",
+            "email" => "admin@gtest.com",
+            "password" => bcrypt("password")
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            "name" => "manager",
+            "role" => "pool_manager",
+            "email" => "manager@gtest.com",
+            "password" => bcrypt("password")
+        ]);
+
+        User::create([
+            "name" => "driver",
+            "role" => "driver",
+            "email" => "driver@gtest.com",
+            "password" => bcrypt("password")
+        ]);
+
+        //SEEDER FOR VEHICLE
+        Vehicle::create([
+            "name" => "people 1",
+            "type" => "people_transport",
+            "status" => "available",
+            "owner" => "company"
+        ]);
+
+        Vehicle::create([
+            "name" => "people 2",
+            "type" => "people_transport",
+            "status" => "available",
+            "owner" => "rent"
+        ]);
+
+        Vehicle::create([
+            "name" => "minging 1",
+            "type" => "mining_transport",
+            "status" => "available",
+            "owner" => "company"
+        ]);
+
+        Vehicle::create([
+            "name" => "people 2",
+            "type" => "mining_transport",
+            "status" => "available",
+            "owner" => "rent"
+        ]);
+
+        //SEEDER FOR RENT
     }
 }
